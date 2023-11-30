739c2ba89539cd31a0454b24baf45b18c1b09a78
Charlie Grant <cag9@email.sc.edu>
2023-11-29 21:13:21 -0500

    Began implementing TicTacToe game.

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "../inc/main.hpp"
+
+int main(){
+	std::cout << "Welcom to TicTacToe!" << std::endl;
+	TicTacToe board;
+	board.to_string();
+	// board.playgame();
+	std::cout << "Game Over!" << std::endl;
+	return 0;
+}
